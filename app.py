87632c8e061290bd66f602fe0dc074dd74a18e2e
Ahmad Romman <rommaneus@hotmail.com>
2025-05-26 12:32:34 +0300

    adding full name

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
         return render_template('greet.html', name=name)
     return render_template('home.html')
 
+@app.route('/full-name')
+def fullname():
+    return render_template('fullname.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
